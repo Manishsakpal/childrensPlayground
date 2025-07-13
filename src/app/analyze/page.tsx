eb20b58df3fb6a70bb10c9bb9912d5ce0d654ee0
Manish anil Sakpal <sakpalmanish01@gmail.com>
2025-07-13 14:50:59 +0000

    remove un wanted code peace please check before with find if that code i

--- src/app/analyze/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AnalyzePage() {
-  return (
-    <div className="container mx-auto p-4 flex justify-center items-center h-[calc(100vh-80px)]">
-      <h1 className="text-2xl text-muted-foreground">The Analyze page is empty.</h1>
-    </div>
-  );
-}
